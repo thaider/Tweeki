0c4ff56f90229b1364b623688e266459893f002e
Florian K. Martin <fkm.dev@gmail.com>
2019-03-19 14:49:23 +0100

    Migrates ./components/bs4.css styles.

--- resources/scripts/tweeki.js
+++ resources/scripts/tweeki.js
@@ -111,41 +111,44 @@ jQuery(document).ready(function ($) {
   // don't close dropdown when clicking in the login form
   $( "#loginext" ).click( function( e ) {
     e.stopPropagation();
-    });
+  });
+
   // focus user name field
   $( "#n-login-ext" ).click( function() {
     if( ! $( this ).parent().hasClass( "open" ) ) {
       setTimeout( '$( "#wpName2" ).focus();', 100 );
-      }
-    });
+    }
+  });
 
-        /**
-         * Fix VisualEditor scroll stickiness
-         *
-         * Had to use the child-parent methods below because the oo-ui-toolbar-bar
-         * class exists on multiple divs.
-         *
-         * The code calculates the navbar height and uses that number as the 'top'
-         * CSS attribute. This calculation is probably moot as it doesn't appear
-         * that the skin, or VisualEditor plays well on screen resolutions less
-         * than 1024 pixels wide. Left the code this way in case something with
-         * VE changes in the future.
-         *
-         **/
-         $(window).scroll( function ( e ) {
-                 // Check to see if the navbar-fixed-top class exists. If it
-                 // does then the navbar is fixed and run this code if
-                 if ( $( '.navbar-fixed-top').length ) {
-                         var $el = $('.oo-ui-toolbar-bar > .oo-ui-toolbar-actions');
-                         var $headerheight = $('#mw-head').height();
-                         var isPositionFixed = ($el.parent().css('position') == 'fixed');
-                         if ($(this).scrollTop() > $headerheight && !isPositionFixed){
-                                 $el.parent().css( 'top', $headerheight );
-                         }
-                         if ($(this).scrollTop() < $headerheight )
-                         {
-                                 $el.parent().css( 'top', '');
-                         }
-                 }
-              });
+  /**
+   * Fix VisualEditor scroll stickiness
+   *
+   * Had to use the child-parent methods below because the oo-ui-toolbar-bar
+   * class exists on multiple divs.
+   *
+   * The code calculates the navbar height and uses that number as the 'top'
+   * CSS attribute. This calculation is probably moot as it doesn't appear
+   * that the skin, or VisualEditor plays well on screen resolutions less
+   * than 1024 pixels wide. Left the code this way in case something with
+   * VE changes in the future.
+   *
+   **/
+   $(window).scroll( function ( e ) {
+     // Check to see if the navbar-fixed-top class exists. If it
+     // does then the navbar is fixed and run this code if
+     if ( $( '.navbar-fixed-top').length ) {
+      var $el = $('.oo-ui-toolbar-bar > .oo-ui-toolbar-actions');
+      var $headerheight = $('#mw-head').height();
+      var isPositionFixed = ($el.parent().css('position') == 'fixed');
+
+      if ($(this).scrollTop() > $headerheight && !isPositionFixed){
+        $el.parent().css( 'top', $headerheight );
+      }
+
+      if ($(this).scrollTop() < $headerheight )
+      {
+        $el.parent().css( 'top', '');
+      }
+     }
+  });
 });
